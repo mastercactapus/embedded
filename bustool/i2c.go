aeb8b494f7890e68e5d33ed0dc642fb843aa2f23
Nathaniel Caza <mastercactapus@gmail.com>
2022-03-01 08:26:23 -0600

    add read/write commands

--- bustool/i2c.go
+++ bustool/i2c.go
@@ -1,6 +1,7 @@
 package bustool
 
 import (
+	"encoding/hex"
 	"fmt"
 
 	"github.com/mastercactapus/embedded/i2c"
@@ -18,7 +19,13 @@ func AddI2C(sh *term.Shell, sda, scl i2c.Pin) {
 		return nil
 	}})
 
-	i2cSh.AddCommand(term.Command{Name: "ping", Desc: "Ping a device.", Exec: func(c *term.CmdCtx) error {
+	for _, c := range i2cCommands {
+		i2cSh.AddCommand(c)
+	}
+}
+
+var i2cCommands = []term.Command{
+	{Name: "ping", Desc: "Ping a device.", Exec: func(c *term.CmdCtx) error {
 		addr := c.FlagByte(term.Flag{Name: "d", Env: "DEVICE", Desc: "Device addresss.", Req: true})
 		write := c.FlagBool(term.Flag{Name: "w", Desc: "Ping the write address instead."})
 		if err := c.Parse(); err != nil {
@@ -35,9 +42,9 @@ func AddI2C(sh *term.Shell, sda, scl i2c.Pin) {
 		}
 
 		return bus.Ping(addr)
-	}})
+	}},
 
-	i2cSh.AddCommand(term.Command{Name: "scan", Desc: "Scan for devices.", Exec: func(c *term.CmdCtx) error {
+	{Name: "scan", Desc: "Scan for devices.", Exec: func(c *term.CmdCtx) error {
 		if err := c.Parse(); err != nil {
 			return err
 		}
@@ -55,7 +62,7 @@ func AddI2C(sh *term.Shell, sda, scl i2c.Pin) {
 				continue
 			}
 
-			p.Printf("%02x: ", i)
+			p.Printf("0x%02x: ", i)
 			switch {
 			case canRead && canWrite:
 				p.Printf("RW")
@@ -73,5 +80,56 @@ func AddI2C(sh *term.Shell, sda, scl i2c.Pin) {
 		}
 
 		return nil
-	}})
+	}},
+
+	{Name: "w", Desc: "Write to an I2C device register.", Exec: func(c *term.CmdCtx) error {
+		addr := c.FlagByte(term.Flag{Name: "d", Env: "DEVICE", Desc: "Device addresss.", Req: true})
+		reg := c.FlagByte(term.Flag{Name: "r", Def: "0", Env: "REGISTER", Desc: "Register address."})
+		str := c.FlagString(term.Flag{Name: "s", Env: "DATA", Desc: "Value to write."})
+		data := c.ArgByteN(term.Arg{Name: "data", Desc: "Value to write."})
+		if err := c.Parse(); err != nil {
+			return err
+		}
+
+		if len(str) > 0 && len(data) > 0 {
+			return term.UsageError("cannot specify both -s and data")
+		}
+		if len(str) > 0 {
+			data = []byte(str)
+		}
+		if len(data) == 0 {
+			return term.UsageError("data or -s required")
+		}
+
+		bus, ok := c.Get("i2c").(*i2c.I2C)
+		if !ok {
+			return fmt.Errorf("i2c: not available")
+		}
+
+		_, err := bus.WriteRegister(addr, reg, data)
+		return err
+	}},
+
+	{Name: "r", Desc: "Read from an I2C device register.", Exec: func(c *term.CmdCtx) error {
+		addr := c.FlagByte(term.Flag{Name: "d", Env: "DEVICE", Desc: "Device addresss.", Req: true})
+		reg := c.FlagByte(term.Flag{Name: "r", Def: "0", Env: "REGISTER", Desc: "Register address.", Req: true})
+		count := c.FlagInt(term.Flag{Name: "n", Def: "0", Desc: "Number of bytes to read."})
+		if err := c.Parse(); err != nil {
+			return err
+		}
+
+		bus, ok := c.Get("i2c").(*i2c.I2C)
+		if !ok {
+			return fmt.Errorf("i2c: not available")
+		}
+
+		r := make([]byte, count)
+		_, err := bus.ReadRegister(addr, reg, r)
+		if err != nil {
+			return err
+		}
+
+		c.Printer().Println(hex.Dump(r))
+		return nil
+	}},
 }
