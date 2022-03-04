caca193a60ecae7cbce10c7f226df6f7f38aa93c
Nathaniel Caza <mastercactapus@gmail.com>
2022-03-04 15:23:03 -0600

    10-bit address support

---
+++ i2c/device.go
@@ -0,0 +1,69 @@
+package i2c
+
+type Bus interface {
+	Tx(addr uint16, w, r []byte) error
+}
+
+type Device struct {
+	bus  Bus
+	addr uint16
+}
+type WriterTo interface {
+	WriteTo([]byte, uint16) (int, error)
+}
+type ReaderFrom interface {
+	ReadFrom([]byte, uint16) (int, error)
+}
+type (
+	ByteWriterTo   interface{ WriteByteTo(byte, uint16) error }
+	ByteReaderFrom interface{ ReadByteFrom(uint16) (byte, error) }
+)
+
+// NewDevice returns a new Device with the given bus and address.
+func NewDevice(bus Bus, addr uint16) *Device {
+	return &Device{bus, addr}
+}
+
+func (d *Device) Write(w []byte) (int, error) {
+	if wt := d.bus.(WriterTo); wt != nil {
+		return wt.WriteTo(w, d.addr)
+	}
+
+	err := d.bus.Tx(d.addr, w, nil)
+	if err != nil {
+		return 0, err
+	}
+
+	return len(w), nil
+}
+
+func (d *Device) Read(r []byte) (int, error) {
+	if rf := d.bus.(ReaderFrom); rf != nil {
+		return rf.ReadFrom(r, d.addr)
+	}
+
+	err := d.bus.Tx(d.addr, nil, r)
+	if err != nil {
+		return 0, err
+	}
+	return len(r), nil
+}
+
+func (d *Device) WriteByte(b byte) error {
+	if w := d.bus.(ByteWriterTo); w != nil {
+		return w.WriteByteTo(b, d.addr)
+	}
+
+	_, err := d.Write([]byte{b})
+	return err
+}
+
+func (d *Device) ReadByte() (byte, error) {
+	if r := d.bus.(ByteReaderFrom); r != nil {
+		return r.ReadByteFrom(d.addr)
+	}
+
+	var b [1]byte
+	_, err := d.Read(b[:])
+	return b[0], err
+}
--- i2c/i2c.go
+++ i2c/i2c.go
@@ -26,6 +26,11 @@ type Config struct {
 	Baudrate uint32
 }
 
+var (
+	ErrBadAddr = errors.New("i2c: bad address")
+	ErrNack    = errors.New("i2c: NACK")
+)
+
 func (i2c *I2C) Configure(config Config) error {
 	if config.Baudrate == 0 {
 		config.Baudrate = 100e3
@@ -56,17 +61,112 @@ func (i2c *I2C) clockUp() {
 	}
 }
 
-func (i2c *I2C) WriteTo(p []byte, addr byte) (int, error) {
+const (
+	Min7BitAddr = 0x08
+	Max7BitAddr = 0x77
+
+	Min10BitAddr = 0x7800
+	Max10BitAddr = 0x7bff
+
+	modeRead  = 1
+	modeWrite = 0
+)
+
+const _10BitAddr = 0b11110_0000000000
+
+func (i2c *I2C) writeLongAddress(addr uint16, mode byte) error {
+	// first byte as write
+	err := i2c.WriteByte(byte(addr>>7) & ^byte(1))
+	if err != nil {
+		return err
+	}
+	err = i2c.WriteByte(byte(addr & 0xff))
+	if err != nil {
+		return err
+	}
+	if mode == modeWrite {
+		return nil
+	}
+
+	// read mode, retransmit first byte after start
+	// with read flag set
+	i2c.Start()
+	err = i2c.WriteByte(byte(addr>>7) | 1)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (i2c *I2C) writeAddress(addr uint16, mode byte) error {
+	if addr < Min7BitAddr {
+		// 7 not allowed
+		// special cases, not suitible for normal addressing
+		return ErrBadAddr
+	}
+
+	if addr > Max7BitAddr && addr < Min10BitAddr {
+		// outside of 10-bit space
+		return ErrBadAddr
+	}
+
+	if addr > Max10BitAddr {
+		return ErrBadAddr
+	}
+
+	if addr > Max7BitAddr {
+		return i2c.writeLongAddress(addr, mode)
+	}
+
+	return i2c.WriteByte(byte(addr<<1) | mode)
+}
+
+func (i2c *I2C) WriteByteTo(p byte, addr uint16) error {
 	i2c.Start()
 	defer i2c.Stop()
 
-	if err := i2c.WriteByte(addr << 1); err != nil {
+	if err := i2c.writeAddress(addr, modeWrite); err != nil {
+		return err
+	}
+
+	return i2c.WriteByte(p)
+}
+
+func (i2c *I2C) WriteTo(p []byte, addr uint16) (int, error) {
+	i2c.Start()
+	defer i2c.Stop()
+
+	if err := i2c.writeAddress(addr, modeWrite); err != nil {
 		return 0, err
 	}
 
 	return i2c.Write(p)
 }
 
+func (i2c *I2C) ReadFrom(p []byte, addr uint16) (int, error) {
+	i2c.Start()
+	defer i2c.Stop()
+
+	if err := i2c.writeAddress(addr, modeRead); err != nil {
+		return 0, err
+	}
+
+	return i2c.Read(p)
+}
+
+func (i2c *I2C) ReadByteFrom(addr uint16) (byte, error) {
+	i2c.Start()
+	defer i2c.Stop()
+
+	if err := i2c.writeAddress(addr, modeRead); err != nil {
+		return 0, err
+	}
+
+	return i2c.ReadByte()
+}
+
+// Write will write directly to the bus, without any address or start/stop condition.
 func (i2c *I2C) Write(p []byte) (int, error) {
 	for i, b := range p {
 		if err := i2c.WriteByte(b); err != nil {
@@ -76,17 +176,7 @@ func (i2c *I2C) Write(p []byte) (int, error) {
 	return len(p), nil
 }
 
-func (i2c *I2C) ReadFrom(p []byte, addr byte) (int, error) {
-	i2c.Start()
-	defer i2c.Stop()
-
-	if err := i2c.WriteByte((addr << 1) | 1); err != nil {
-		return 0, err
-	}
-
-	return i2c.Read(p)
-}
-
+// Read will read directly from the bus, without any address or start/stop condition.
 func (i2c *I2C) Read(p []byte) (int, error) {
 	buf := p
 	for len(buf) > 0 {
@@ -103,6 +193,7 @@ func (i2c *I2C) Read(p []byte) (int, error) {
 	return len(p), nil
 }
 
+// ReadByte reads a single byte directly from the bus, without any address or start/stop condition.
 func (i2c *I2C) ReadByte() (byte, error) {
 	var b byte
 	for i := 0; i < 8; i++ {
@@ -118,6 +209,7 @@ func (i2c *I2C) ReadByte() (byte, error) {
 	return b, nil
 }
 
+// WriteByte writes a single byte to the bus, without any address or start/stop condition.
 func (i2c *I2C) WriteByte(b byte) error {
 	for i := 0; i < 8; i++ {
 		i2c.writeBit(((b >> (7 - i)) & 1) == 1)
@@ -132,8 +224,7 @@ func (i2c *I2C) WriteByte(b byte) error {
 	return nil
 }
 
-var ErrNack = errors.New("i2c: NACK")
-
+// Start will send a start condition on the bus.
 func (i2c *I2C) Start() {
 	i2c.clockUp()
 	wait()
@@ -168,6 +259,7 @@ func (i2c *I2C) readBit() (value bool) {
 	return value
 }
 
+// Stop will send a stop condition on the bus.
 func (i2c *I2C) Stop() {
 	i2c.sda.OutputLow()
 	wait()
